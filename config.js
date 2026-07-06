17be1654bf6d0751bfa69d879c40af0b87e77331
Chris <c_booher@hotmail.com>
2026-07-06 07:53:09 -0400

    Restore API keys

--- config.js
+++ config.js
@@ -6,9 +6,9 @@
 
 const ACES_CONFIG = {
   supabaseUrl:  'https://urwwzdlkgfljfvqgfhyb.supabase.co',
-  supabaseKey:  'eyJhbG...',
+  supabaseKey:  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyd3d6ZGxrZ2ZsamZ2cWdmaHliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxMzY0MzYsImV4cCI6MjA5NzcxMjQzNn0.s5eOVqI0EaCnP5jDfuVtHeqBonsaIwwqQBrf1heMK1k',
   ytChannelId:  'UC6nY8Jns2vJYu3ZyDTUDnJw',
-  ytApiKey:     'AIzaSyDqx...',
+  ytApiKey:     'AIzaSyDqxQVhuhZHHaycgMA34vqfemQnqAAy_RQ',
   teamId:       'a0000000-0000-0000-0000-000000000001',
   pin:          '2000',
   activeSeason: 'spring2026'
